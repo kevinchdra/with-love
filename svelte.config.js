ee3a624e0eb1b1baaf5b9416a2c8904c808d288b
Kevin <kevinchdra21@gmail.com>
2025-08-17 01:16:37 +0700

    HandleSettingsClick Removed

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import vercel from '@sveltejs/adapter-vercel';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
