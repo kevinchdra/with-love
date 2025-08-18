5d2b6ca433d2ddc7acb3362d16aa5c0a28fd4ebe
Kevin <kevinchdra21@gmail.com>
2025-08-18 16:37:02 +0700

    rsvp scaling

--- src/lib/components/personalrsvp.svelte
+++ src/lib/components/personalrsvp.svelte
@@ -373,7 +373,7 @@
 }
 </style>
 
-<section class="min-h-[100dvh] rsvp-section flex flex-col justify-center items-center px-8 text-white">
+<section class="min-h-[100dvh] rsvp-section flex flex-col justify-center items-center px-8 md:px-16 lg:px-32 text-white">
   <div class="max-w-xl w-full space-y-6 fade-in">
     <div class="text-center space-y-4">
       <p class="uppercase font-smallcaption mb-6">Reserve your seat</p>
